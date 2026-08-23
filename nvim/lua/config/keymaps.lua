9f912798452ab214f7924f8c32e1aaab82b91179
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2026-08-23 10:26:17 -0500

    feat(nvim): add keymaps to move between buffers

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = " " 
 
 function set(mode, keys, action, desc) 
-	mode = mode or "n"
 	desc = desc or ""
 
 	vim.keymap.set(mode, keys, action, { desc = desc })
@@ -10,8 +9,9 @@ end
 set("n", "<leader>w", "<cmd>w<cr>")
 set("n", "<leader>q", "<cmd>q<cr>")
 set("n", "j", "jzz")
-set("i", "jj", "<Esc>")
 set("n", "k", "kzz")
+set("i", "jj", "<Esc>")
 set("n", "G", "Gzz")
 
-
+set("n", "<S-l>", "<cmd>bnext<cr>", "go to next buffer")
+set("n", "<S-h>", "<cmd>bprevious<cr>", "go to previous buffer")
