fa5fc6460001061ecbdba48231d8922d9981b9b9
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2026-09-11 20:50:02 -0500

    feat(nvim): add keymap to see diagnostics

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,5 +1,22 @@
 -- enabling servers
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("basedpyright")
-vim.lsp.enable("emmet_ls")
-vim.lsp.enable("ts_ls")
+vim.lsp.enable("emmet_language_server")
+vim.lsp.enable("vtsls")
+vim.lsp.enable("cssls")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UsrLSPConfig", {}), 
+
+	callback = function()
+		-- binds 
+		function map(mode, keys, action, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+
+			vim.keymap.set(mode, keys, action, opts)
+		end
+
+		map("n", "<leader>ld", vim.diagnostic.open_float, { desc = "open diagnostics with floating window" })
+	end
+})
