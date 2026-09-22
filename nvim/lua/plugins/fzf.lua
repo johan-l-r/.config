f4baee62d0307619e12008f98f62fc4c6214aad8
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2026-09-21 22:29:43 -0500

    nvim: set preview layout to horizontal

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -5,10 +5,7 @@ return {
 
 	config = function()
 		local fzf = require("fzf-lua")
-
-		local function set(mode, keys, action, desc)
-			vim.keymap.set(mode, keys, action, { desc = desc })
-		end
+		local map = vim.keymap
 		
 		fzf.setup({
 			file_icon_padding = "5", 
@@ -19,18 +16,19 @@ return {
 				border = "single", 
 
 				preview = {
+					layout = "horizontal", 
 					border = "single"
 				}
 			}, 
 
-			set("n", "<leader>ff", function() fzf.files() end, "find files"),
-			set("n", "<leader>fb", function() fzf.buffers() end, "find buffers"),
+			map.set("n", "<leader>ff", function() fzf.files() end, { desc = "find files" }),
+			map.set("n", "<leader>fb", function() fzf.buffers() end, { desc = "find buffers" }),
 
-			set("n", "<leader>g", function() fzf.grep() end, "find pattern with grep"), 
-			set("n", "<leader>G", function() fzf.grep_cword() end, "find pattern under cursor with grep"), 
+			map.set("n", "<leader>fg", function() fzf.grep() end, { desc = "find pattern with grep" }), 
+			map.set("n", "<leader>fc", function() fzf.grep_cword() end, { desc = "find pattern under cursor with grep" }), 
 			
-			set("n", "<leader>gc", function() fzf.git_commits() end, "find pattern with grep"),
-			set("n", "<leader>gb", function() fzf.git_branches() end, "find pattern with grep")
+			map.set("n", "<leader>fgc", function() fzf.git_commits() end, { desc = "find pattern with grep" }),
+			map.set("n", "<leader>fgb", function() fzf.git_branches() end, { desc = "find pattern with grep" })
 		})
 	end
 }
